25f46c85e78df9c7971b860446e175a93bfc83df
Shakhzod <azamatjonov90@gmail.com>
2022-05-07 16:43:06 +0500

    Commit2: Added label

--- IamStupid/ViewController.swift
+++ IamStupid/ViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        self.label.text = "Normal"
+        
     }
     
 
